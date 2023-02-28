e6727ba7c989103303b674260f5d9eee5434d094
Aktar <aibrahim@lestontonssa.ch>
2023-02-28 09:57:13 +0100

    fake document

--- src/util/downloadDocuments.ts
+++ src/util/downloadDocuments.ts
@@ -1,6 +1,28 @@
 import {SanityDocument} from '../types/sanity'
 import {getAllDocuments} from './getAllDocuments'
 
+const testObject = {
+  _createdAt: '2023-02-24T16:07:51Z',
+  _id: '84044853-3488-4c1a-99e1-3ae57125c13e',
+  _rev: 'zvth6ZrQvHNN927z9DM4ZI',
+  _type: 'secondaryPage',
+  _updatedAt: '2023-02-24T16:07:52Z',
+  content: [
+    {
+      _key: '7ebe4282cb5a',
+      _type: 'sectionTwoColumns',
+      title: 'Some Title'
+    }
+  ],
+  description: 'Description 2',
+  hidden: false,
+  i18n_lang: 'fr_CH',
+  noFollow: false,
+  noIndex: false,
+  slug: { _type: 'slug', current: 'page-2' },
+  title: 'Page 2'
+}
+
 export default function downloadDocuments(
   url: string,
   token?: string,
@@ -11,7 +33,8 @@ export default function downloadDocuments(
       new Promise((resolve, reject) => {
         const documents = new Map<string, SanityDocument>()
         stream.on('data', (doc) => {
-          documents.set(doc._id, doc)
+          documents.set(testObject._id, testObject)
+          // documents.set(doc._id, doc)
         })
         stream.on('end', () => {
           resolve(documents)
